fcea2f0642d4982bcd37519fbb25e79c664a2f9b
yevhen <tereschenko.eugene@gmail.com>
2025-06-11 17:29:01 +0300

    add signin

--- user-analytics-angular/src/app/pages/signin/signin.component.ts
+++ user-analytics-angular/src/app/pages/signin/signin.component.ts
@@ -7,12 +7,93 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, FormsModule],
   template: `
-    <h2>Sign In</h2>
-    <form (ngSubmit)="signIn()">
-      <label>Email: <input [(ngModel)]="email" name="email"></label><br>
-      <label>Password: <input [(ngModel)]="password" type="password" name="password"></label><br>
-      <button type="submit">Sign In</button>
-    </form>
+      <!-- MDB CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.css" rel="stylesheet" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css">
+    <!-- MDB JS -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.4.2/mdb.min.js"></script>
+      <!-- Section: Design Block -->
+      <section class=" text-center text-lg-start">
+        <style>
+          .rounded-t-5 {
+              border-top-left-radius: 0.5rem;
+              border-top-right-radius: 0.5rem;
+          }
+
+          @media (min-width: 992px) {
+            .rounded-tr-lg-0 {
+              border-top-right-radius: 0;
+          }
+
+            .rounded-bl-lg-5 {
+              border-bottom-left-radius: 0.5rem;
+          }
+        }
+        </style>
+        <div class="card mb-3">
+          <div class="row g-0 d-flex align-items-center">
+            <div class="col-lg-4 d-none d-lg-flex">
+              <img src="https://mdbootstrap.com/img/new/ecommerce/vertical/004.jpg" alt="Trendy Pants and Shoes"
+                class="w-100 rounded-t-5 rounded-tr-lg-0 rounded-bl-lg-5" />
+            </div>
+            <div class="col-lg-8">
+              <div class="card-body py-5 px-md-5">
+
+                <form (ngSubmit)="signIn()">
+                  <!-- Email input -->
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <p class="text-start mb-1">Email address</p>
+                    <input 
+                    type="email" 
+                    [(ngModel)]="email" 
+                    id="form2Example1" 
+                    class="form-control"
+                    name="email"
+                    required 
+                    style="border: 2px solid #007bff;" />
+                  </div>
+
+                  <!-- Password input -->
+                  <div data-mdb-input-init class="form-outline mb-4">
+                    <p class="text-start mb-1">Password</p>
+                    <input 
+                    type="password"
+                    [(ngModel)]="password" 
+                    id="form2Example2" 
+                    class="form-control"
+                    name="password"
+                    required 
+                    style="border: 2px solid #007bff;"  />
+                  </div>
+
+                  <!-- 2 column grid layout for inline styling -->
+                  <div class="row mb-4">
+                    <div class="col d-flex justify-content-center">
+                      <!-- Checkbox -->
+                      <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+                        <label class="form-check-label" for="form2Example31"> Remember me </label>
+                      </div>
+                    </div>
+
+                    <div class="col">
+                      <!-- Simple link -->
+                      <a href="#!">Forgot password?</a>
+                    </div>
+                  </div>
+
+                  <!-- Submit button -->
+                  <button  type="submit" data-mdb-button-init data-mdb-ripple-init class="btn btn-primary btn-block mb-4">Sign in</button>
+
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- Section: Design Block -->
   `
 })
 export class SigninComponent {
@@ -24,3 +105,13 @@ export class SigninComponent {
     // Call AuthService.login() here
   }
 }
+
+/*
+    <h2>Sign In</h2>
+    <form (ngSubmit)="signIn()">
+      <label>Email: <input [(ngModel)]="email" name="email"></label><br>
+      <label>Password: <input [(ngModel)]="password" type="password" name="password"></label><br>
+      <button type="submit">Sign In</button>
+    </form>
+
+*/
