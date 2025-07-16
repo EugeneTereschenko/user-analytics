e3930f549b8432387d9787b9c2fe2305f66ea449
yevhen <tereschenko.eugene@gmail.com>
2025-07-16 17:50:36 +0300

    add files

--- user-analytics-angular/src/app/pages/tasks/tasks.component.ts
+++ user-analytics-angular/src/app/pages/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TaskService } from '../../task.service';
+import { RouterModule } from '@angular/router';
 
 interface Task {
   id: number;
@@ -19,14 +21,41 @@ export class TasksComponent {
   newTask = '';
   tasks: Task[] = [];
 
+  constructor(private taskService: TaskService) { }
+
+
+  ngOnInit() {
+    console.log('TasksComponent initialized');
+    this.refresh();
+  }
+
   addTask() {
     if (!this.newTask.trim()) return;
     const newId = this.tasks.length ? this.tasks[this.tasks.length - 1].id + 1 : 1;
     this.tasks.push({ id: newId, title: this.newTask, done: false });
-    this.newTask = '';
+    //this.newTask = '';
+    this.taskService.createTask({ id: newId, title: this.newTask, done: false }).subscribe({
+      next: (task: Task) => {
+        console.log('Task created successfully', task);
+      },
+      error: (err: any) => {
+        console.error('Failed to create task', err);
+      }
+    });
   }
 
   deleteTask(id: number) {
     this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  refresh() {
+    this.taskService.getTasks().subscribe({ 
+      next: (tasks: Task[]) => {
+        this.tasks = tasks;
+      },
+      error: (err: any) => {
+        console.error('Failed to fetch tasks', err);
+      }
+    });
   }
 }
