85ba71a7de69e67dde69525203c43d46d620c50d
yevhen <tereschenko.eugene@gmail.com>
2025-07-22 16:19:50 +0300

    add files

--- user-analytics-angular/src/app/app.component.ts
+++ user-analytics-angular/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from './menu/menu.component';
+import { ThemeToggleComponent } from './shared/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MenuComponent, RouterOutlet, RouterModule, CommonModule],
+  imports: [MenuComponent, RouterOutlet, RouterModule, CommonModule, ThemeToggleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
