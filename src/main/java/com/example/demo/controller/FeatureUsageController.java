c51c493629ec86a9e4344155b389f9e4c4e58a6b
yevhen <tereschenko.eugene@gmail.com>
2025-08-21 17:43:25 +0300

    add FeatureService

--- src/main/java/com/example/demo/controller/FeatureUsageController.java
+++ src/main/java/com/example/demo/controller/FeatureUsageController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.FeatureUsageDTO;
+import com.example.demo.service.impl.FeatureService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @Slf4j
 @RestController
@@ -16,12 +15,11 @@ import java.util.List;
 @RequestMapping("/api/usage/feature")
 public class FeatureUsageController {
 
+    private final FeatureService featureService;
+
     @GetMapping
     public ResponseEntity<?> getFeatureUsage() {
-        return ResponseEntity.ok(new FeatureUsageDTO.Builder()
-                        .features(List.of("Feature A", "Feature B", "Feature C"))
-                        .usageCounts(List.of(100, 200, 150))
-                        .build());
+        return ResponseEntity.ok(featureService.getFeatureUsage());
     }
 }
 
