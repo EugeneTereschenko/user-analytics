01fe5a909c42af3b86b9f7785501ea131414031c
yevhen <tereschenko.eugene@gmail.com>
2025-12-02 17:30:46 +0200

    fix http provider and auth

--- user-analytics-angular/src/main.ts
+++ user-analytics-angular/src/main.ts
@@ -3,12 +3,15 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, HttpClientModule, withFetch } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-// Factory function for the loader
+
+console.log("BOOTSTRAPPING WITH PROVIDERS…");
+
+// loader factory
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
@@ -16,8 +19,11 @@ export function HttpLoaderFactory(http: HttpClient) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    provideHttpClient(withFetch())
+,
+
     importProvidersFrom(
-      HttpClientModule,
+      HttpClientModule,                // ✅ REQUIRED for ngx-translate
       TranslateModule.forRoot({
         loader: {
           provide: TranslateLoader,
@@ -26,6 +32,7 @@ bootstrapApplication(AppComponent, {
         }
       })
     ),
+
     provideAnimationsAsync()
   ]
 });
