a2e940c20942a62415d323ed4c7fccb2cdbbae82
yevhen <tereschenko.eugene@gmail.com>
2025-12-29 16:03:22 +0200

    fix import

--- user-analytics/medicalrecordservice/src/main/java/com/healthcare/medicalrecordservice/exception/GlobalExceptionHandler.java
+++ user-analytics/medicalrecordservice/src/main/java/com/healthcare/medicalrecordservice/exception/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ package com.healthcare.medicalrecordservice.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
